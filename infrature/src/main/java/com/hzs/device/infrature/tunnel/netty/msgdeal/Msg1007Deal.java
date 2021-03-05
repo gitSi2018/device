c6393076374ac1ea464b875a9d3bb008eb1809a6
hongzhensi <1962796858@qq.com>
2021-03-05 11:25:28 +0800

    1007消息处理

---
+++ infrature/src/main/java/com/hzs/device/infrature/tunnel/netty/msgdeal/Msg1007Deal.java
@@ -0,0 +1,48 @@
+package com.hzs.device.infrature.tunnel.netty.msgdeal;
+
+import com.hzs.device.common.msgin.BaseMsgIn;
+import com.hzs.device.common.msgin.msg.ConnectMsg;
+import com.hzs.device.infrature.tunnel.netty.msgout.CommonResponse;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author: HongZhenSi
+ * @date: 2021/3/5
+ * @modifiedBy:
+ * @description:
+ * @version: 1.0
+ */
+@Slf4j
+@Component
+public class Msg1007Deal extends ConnectMsgDeal {
+
+    @Resource
+    private CommonResponse commonResponse;
+
+    @Override
+    public String getPoint() {
+        return "1007";
+    }
+
+    @Override
+    public BaseMsgIn getData(List<Integer> msg) {
+        return null;
+    }
+
+    @Override
+    public boolean execute(List<Integer> msg, ChannelHandlerContext context) {
+
+        log.info("Msg1007Deal execute, msg:{}", msg);
+        ConnectMsg connectMsg = convert(msg, context);
+
+
+        sendToDevice(commonResponse.getMsgData(connectMsg.getDeviceId(), msg.get(11), msg.get(12), 0x10, 0x07, 0)
+                , connectMsg.getChannel());
+        return true;
+    }
+}
